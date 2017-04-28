f84669f93117a7fe9527a9bfac83219982793d12
petermonte <Peter@Peter.home>
2017-04-28 18:58:09 +0100

    Implement correctly and only chrome prefix in css properties

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -40,7 +40,7 @@ var processSassFiles = function(src, dest, filename, concatenate, map, min) {
                 outputStyle: 'compressed',
                 errLogToConsole: true
             }).on('error', error.reportError ))
-        .pipe(prefix("last 1 version", "> 1%"))
+        .pipe(prefix("Chrome"))
         .pipe(gulpif(concatenate, concat(filename)))
         .pipe(gulpif(config.appendDotMin && min, rename({ suffix: '.min' })))
         .pipe(gulpif(map, sourcemaps.write(".")))
