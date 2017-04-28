ee24ab414f0e420f2bf3ddb56ecfd987726da0d2
petermonte <Peter@Peter.home>
2017-04-28 18:57:31 +0100

    Several changes
    Cleans file. Removes API call. Applies closest city name. Moves powered
    by to top. Removes vendors.js - no third-party library in use for now.

--- index.php
+++ index.php
@@ -1,43 +1,5 @@
 <?php
 /**
- * Weather Forecast App for Screenly devices.
- *
- * @category ScreenlyApps
- * @package  WeatherForecast
- * @author   Original Peter Monte <pmonte@screenly.io>
- * @license  https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html  GPLv2
- * @link     https://github.com/wireload
- * @since    0.0.1
- */
-
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-
-
-/**
- * Get current location
- *
- * @category ScreenlyApps
- * @package  WeatherForecast
- * @author   Original Peter Monte <pmonte@screenly.io>
- * @license  https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html  GPLv2
- * @link     https://github.com/wireload
- * @since    0.0.1
- */
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-$geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$ip"));
-
-
-
-/**
  * Get current weather conditions
  *
  * @category ScreenlyApps
@@ -47,17 +9,30 @@ $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$ip"));
  * @link     https://github.com/wireload
  * @since    0.0.1
  */
-include_once 'lib/forecast.io.php';
 
-$forecast = new ForecastIO('280b615c4a69fe34235855040fd4dccc');
-$condition = $forecast->getCurrentConditions($geo['geoplugin_latitude'], $geo['geoplugin_longitude']);
+include_once 'forecast.php';
 $raw  = (object)$forecast->getRaw ();
 $week = (array)$raw->daily->data;
-$data = $week[0];
-date_default_timezone_set($raw->timezone);
+$today = $week[0];
+
+
+
+/**
+ * Set the default timezone based on $lat $lng result
+ *
+ * @category ScreenlyApps
+ * @package  WeatherForecast
+ * @author   Original Peter Monte <pmonte@screenly.io>
+ * @license  https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html  GPLv2
+ * @link     https://github.com/wireload
+ * @since    0.0.1
+ */
+// date_default_timezone_set($raw->timezone);
+// setlocale(LC_TIME, 'fr');
+// strftime('%a %d', $today->time);
 ?>
 <!DOCTYPE html>
-    <html id="app" lang="en_UK" class="bg-day" data-sunset="<?php echo $data->sunsetTime ?>" data-sunrise="<?php echo $data->sunriseTime ?>">
+    <html id="app" lang="en_UK" class="bg-day" data-sunset="<?php echo $today->sunsetTime ?>" data-sunrise="<?php echo $today->sunriseTime ?>">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="chrome=1">
@@ -74,14 +49,14 @@ date_default_timezone_set($raw->timezone);
             <div id="current-day" class="left">
                 <!-- CURRENT DAY WITH LOCATION & DATE -->
                 <div id="location-today">
-                    <span class="location"><?php echo preg_replace('/[_]+/', ' ', substr($raw->timezone, strpos($raw->timezone, "/") + 1)); ?>.</span>
-                    <span class="today"><?php echo date('D d', $data->time);?></span>
+                    <span class="location"><?php echo !empty($geoInfo->name) ? $geoInfo->name : $geoInfo->adminName1; ?>, </span>
+                    <span class="today"><?php echo date('D d', $today->time);?></span>
                 </div>
                 <!-- TEMPERATURE - MAX MIN -->
                 <div id="temperatures">
                     <h1 id="temp"><?php echo round($condition->getTemperature()); ?><sup>º</sup></h1>
-                    <p id="temp-max"><?php echo round($data->apparentTemperatureMax); ?>º<b>high</b></p>
-                    <p id="temp-min"><?php echo round($data->apparentTemperatureMin); ?>º<b>low</b></p>
+                    <p id="temp-max"><?php echo round($today->apparentTemperatureMax); ?>º<b>high</b></p>
+                    <p id="temp-min"><?php echo round($today->apparentTemperatureMin); ?>º<b>low</b></p>
                 </div>
                 <div id="weather">
                     <i id="icon_current" class="wi wi-forecast-io-<?php echo $raw->hourly->icon ?>"></i>
@@ -91,6 +66,8 @@ date_default_timezone_set($raw->timezone);
 
             <!-- NEXT DAYS -->
             <div id="next-days" class="right">
+                <p id="app-name">Weather Forecast</p>
+                <small id="poweredby">Powered by Dark Sky</small>
                 <ul>
                     <?php for($i=1;$i<=4;$i++): ?>
                     <li>
@@ -100,8 +77,6 @@ date_default_timezone_set($raw->timezone);
                     </li>
                     <?php endfor; ?>
                 </ul>
-                <p id="app-name">Weather Forecast</p>
-                <small id="poweredby">Weather data by Forecast.io</small>
             </div>
         </section>
 
@@ -111,7 +86,7 @@ date_default_timezone_set($raw->timezone);
                 <svg class="logo" width="70" height="70">
                     <image xlink:href="assets/svg/screenly-logo-symbol.svg" src="assets/svg/screenly-logo-symbol.png" width="70" height="70" />
                 </svg>
-                <span>© <?php echo date('Y');?> Screenly, Inc. Screenly Apps - Weather Forecast.</span>
+                <span>© <?php echo date('Y');?> Screenly, Inc.</span>
             </div>
             <div class="right">
                 <span id="footer-clock">13:23</span>
@@ -119,7 +94,7 @@ date_default_timezone_set($raw->timezone);
         </footer>
 
         <!-- FRONT-END SCRIPTS -->
-        <script type="text/javascript" src="assets/js/vendors/vendors.js"></script>
+        <!-- <script type="text/javascript" src="assets/js/vendors/vendors.js"></script> -->
         <script type="text/javascript" src="assets/js/app.js"></script>
     </body>
 </html>
