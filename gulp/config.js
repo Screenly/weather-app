1cce351bd1f983516dda85fe28aac8812782f2ed
petermonte <Peter@Peter.home>
2017-05-02 12:18:56 +0100

    correct indent

--- gulp/config.js
+++ gulp/config.js
@@ -1,41 +1,41 @@
 var pngquant = require('imagemin-pngquant');
 
 module.exports = {
-	// Image min configuration
-	imagemin: {
+    // Image min configuration
+    imagemin: {
 
-        optimizationLevel: 	5,
-        progressive: 		true,
-        interlaced: 		true,
-        use: 				[pngquant()],
-        svgoPlugins: 		[{removeViewBox: false}]
+        optimizationLevel:  5,
+        progressive:        true,
+        interlaced:         true,
+        use:                [pngquant()],
+        svgoPlugins:        [{removeViewBox: false}]
 
     },
 
-	// -----------------------------------------------------------
+    // -----------------------------------------------------------
 
-	// Path definitions
-	path: {
+    // Path definitions
+    path: {
 
-		ROOT: 				'.',
-		SRC:                './src/',
-		ASSETS: 			'./assets',
-		HTML: 				'./**/*.html',
-		JS_SRC: 			'./src/js',
-		JS_DEST: 			'./assets/js',
-		CSS_SRC: 			'./src/scss',
-		CSS_DEST: 			'./assets/css',
-		JS_ENTRY_POINT: 	'./src/js/app.js',
-		SCSS_ENTRY_POINT: 	'./src/scss/app.scss'
+        ROOT:               '.',
+        SRC:                './src/',
+        ASSETS:             './assets',
+        HTML:               './**/*.html',
+        JS_SRC:             './src/js',
+        JS_DEST:            './assets/js',
+        CSS_SRC:            './src/scss',
+        CSS_DEST:           './assets/css',
+        JS_ENTRY_POINT:     './src/js/app.js',
+        SCSS_ENTRY_POINT:   './src/scss/app.scss'
 
-	},
+    },
 
-	// -----------------------------------------------------------
+    // -----------------------------------------------------------
 
-	// Join Sass & JS Pages into one minified file?
-	joinPages: true,
+    // Join Sass & JS Pages into one minified file?
+    joinPages: true,
 
-	// Append .min to the file url
-	appendDotMin: false,
+    // Append .min to the file url
+    appendDotMin: false,
 
 };
