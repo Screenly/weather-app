e80d50ac236c03bdacb8002ccb37ea3fa8c6a7d2
petermonte <Peter@Peter.home>
2017-05-02 12:35:21 +0100

    adding comment to explain empty js file

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * This file should hold global script. Use app.utils for utilities or create a new
+ * file like app.clock.js for specific tasks.
+ *
+ * @category ScreenlyApps
+ * @package  WeatherForecast
+ * @author   Original Peter Monte <pmonte@screenly.io>
+ * @license  https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html  GPLv2
+ * @link     https://github.com/wireload
+ * @since    0.0.1
+ */
