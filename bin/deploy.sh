6a5e88176903d2d1fd337e2c484e54d94a9dbf7f
Viktor Petersson <vpetersson@wireload.net>
2018-11-01 09:27:53 +0000

    Fixes deploy script.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,3 +5,4 @@ TARGET_USER="deployer"
 TARGET_PATH="/www/weather.srly.io/"
 
 rsync -aP --delete _site/* ${TARGET_USER}@${TARGET}:${TARGET_PATH}
+ssh ${TARGET_USER}@${TARGET} chmod a+rX -R ${TARGET_PATH}
