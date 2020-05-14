133f50457f8378743f7ece10b396aff688a3b83a
Viktor Petersson <vpetersson@screenly.io>
2020-05-14 15:08:20 +0100

    Adds more sentry hook

--- ci/step_3.sh
+++ ci/step_3.sh
@@ -3,6 +3,7 @@
 # -*- sh-basic-offset: 4 -*-
 
 GITBRANCH=$(git rev-parse --abbrev-ref HEAD)
+GITHASH=$(git rev-parse --short HEAD)
 SERVERS=(real.us.hosting.wireload.net)
 
 # Accept SSH keys
@@ -13,6 +14,15 @@ done
 # Deploy to stage
 if [ "$GITBRANCH" == 'master' ]; then
     DSTFOLDER="stage-weather.srly.io"
+
+    # Use stage backend
+    sed -i 's/weather-backend.srly.io/stage-weather-backend.srly.io'
+
+    # Send release to Sentry
+    curl https://sentry.stage.screenlyapp.com/api/hooks/release/builtin/6/262be118d8a5acc15d956804ff422acb116766b609f20f3c376eb9596de2de0b/ \
+        -X POST \
+        -H 'Content-Type: application/json' \
+        -d '{"version": "$GITHASH"}'
 elif [ "$GITBRANCH" == 'production' ]; then
     DSTFOLDER="weather.srly.io"
 else
