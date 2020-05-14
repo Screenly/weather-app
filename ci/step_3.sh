bf38c26bd20977d09ca8e6d5a3e1cee7a343e011
Viktor Petersson <vpetersson@screenly.io>
2020-05-14 16:24:31 +0100

    Removes incorrect Sentry

--- ci/step_3.sh
+++ ci/step_3.sh
@@ -3,7 +3,6 @@
 # -*- sh-basic-offset: 4 -*-
 
 GITBRANCH=$(git rev-parse --abbrev-ref HEAD)
-GITHASH=$(git rev-parse --short HEAD)
 SERVERS=(real.us.hosting.wireload.net)
 
 # Accept SSH keys
@@ -14,12 +13,6 @@ done
 # Deploy to stage
 if [ "$GITBRANCH" == 'master' ]; then
     DSTFOLDER="stage-weather.srly.io"
-
-    # Send release to Sentry
-    curl https://sentry.stage.screenlyapp.com/api/hooks/release/builtin/6/262be118d8a5acc15d956804ff422acb116766b609f20f3c376eb9596de2de0b/ \
-        -X POST \
-        -H 'Content-Type: application/json' \
-        -d "{\"version\": \"$GITHASH\"}"
 
 elif [ "$GITBRANCH" == 'production' ]; then
     DSTFOLDER="weather.srly.io"
