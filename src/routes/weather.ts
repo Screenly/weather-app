81a01d5a7c10be91921205d34919c4a6154e076c
Devan Thakur <devanthakur89@gmail.com>
2022-09-06 23:16:12 +0530

    Removes secret

--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -7,7 +7,7 @@ weather.get('/', async (c) => {
   try {
     const { lat = defaultLocation.lat, lng = defaultLocation.lng } = c.req.query()
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&units=metric&cnt=10&appid=${c.env.OPEN_WEATHER_API_KEY}`
-    //`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=001085696589b9680f971c7d40e8e1f3`
+
     const resp = await fetch(apiUrl)
     const json = await resp.json()
 
