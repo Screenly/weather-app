212057936ccc7c94d56530ab65d9c31b2dbfaec0
Devan Thakur <devanthakur89@gmail.com>
2022-09-13 22:47:09 +0530

    Adds cache hits tracking GA code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,14 @@ import { jsx } from 'hono/jsx'
 import Layout from './Layout'
 import Header from './Header'
 import Footer from './Footer'
-import { sentryIds } from '../constants'
+import { sentryIds, gaIds } from '../constants'
 
 const App = (props) => {
   const { env, lat, lng } = props
   const sentryId = sentryIds[env]
+  const gaId = gaIds[env]
   return (
-    <Layout sentryId={sentryId}>
+    <Layout sentryId={sentryId} gaId={gaId}>
       <div class='content'>
         <Header />
         <Footer />
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,11 +7,20 @@ const Layout = (props) => html`<!DOCTYPE html>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="preload" href="/static/fonts/Barlow-Regular.woff" as="font" />
       <link rel="stylesheet" href="/static/styles/main.css" />
-      <script src="/static/js/main.js" async defer></script>
       <script
         src="https://js.sentry-cdn.com/${props.sentryId}.min.js"
         crossorigin="anonymous"
       ></script>
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=${props.gaId}"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${props.gaId}');
+      </script>
+      <script src="/static/js/main.js" async defer></script>
     </head>
     <body>
       ${props.children}
